c5ea4576adb51b8549f5476faf4d04966c47ce7a
DanielledeAvila <danivandinha1@gmail.com>
2025-02-07 19:33:22 -0300

    subindo sispar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Login from './components/login/Login.jsx'
+
+function App(){
+    return(
+        <>
+        <h1>SISPAR</h1>
+        <Login/>
+        </>
+    )
+}
+
+
+export default App
